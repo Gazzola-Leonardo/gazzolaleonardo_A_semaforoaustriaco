990c086c43ae72105689e7f807da462c9691d27f
Gazzola Leonardo <leonardo.gazzola@barsanti.edu.it>
2019-02-02 12:08:57 +0100

    02/02/2019 - Continuazione del progetto semaforo austriaco, con l’inserimento della seriale.

--- Semaforo_austriaco/Semaforo_austriaco.ino
+++ Semaforo_austriaco/Semaforo_austriaco.ino
@@ -1,15 +1,19 @@
 int rosso1 = 8;
 int giallo1 = 10;
 int verde1 = 12;
-int rosso2 = 5;
-int giallo2 = 3;
-int verde2 = 1;
+int rosso2 = 6;
+int giallo2 = 4;
+int verde2 = 2;
 
-//String incomingByte;
+String inserito;
+int tempoVerde = 0;
+int tempoGiallo = 0;
+int nLampVerde = 0;
+int durataLampVerde = 0;
 
 void setup() {
-  /*Serial.begin(9600);
-  Serial.print("seriale attivata");*/
+  Serial.begin(9600);
+  Serial.println("seriale attivata");
   pinMode(rosso1, OUTPUT);
   pinMode(giallo1, OUTPUT);
   pinMode(verde1, OUTPUT);
@@ -18,25 +22,62 @@ void setup() {
   pinMode(verde2, OUTPUT);
 }
 void loop() {
-  /*while(Serial.available() == 0);
-  if (Serial.available() > 0){
-    incomingByte = Serial.readString();
-    Serial.print("I received");
-    Serial.println(incomingByte.toInt());
-    
-  }*/
+  serialeRichiesta();
   Tempo1();
-  delay(3000);  
+  delay(tempoVerde);  
   lampVerde2();
   Tempo2();
-  delay(700);
+  delay(tempoGiallo);
   Tempo3();
-  delay(3000);
+  delay(tempoVerde);
   lampVerde1();
   Tempo4();
-  delay(700);
+  delay(tempoGiallo);
 }
-
+void serialeRichiesta() {
+  serialeVerde();
+  serialeGiallo();
+  serialeNLampVerde();
+}
+void serialeVerde(){
+  Serial.println("Inserire la durata del verde (e rosso):");
+  while(Serial.available() == 0);
+    if (Serial.available() > 0){
+      inserito = Serial.readString();
+      Serial.println(inserito.toInt());
+      tempoVerde = inserito.toInt();
+  }
+}
+void serialeGiallo(){
+  Serial.println("Inserire la durata del giallo:");
+  while(Serial.available() == 0);
+    if (Serial.available() > 0){
+      inserito = Serial.readString();
+      Serial.println(inserito.toInt());
+      tempoGiallo = inserito.toInt();
+    }
+}
+void serialeNLampVerde(){
+  Serial.println("Inserire quanti lampeggi deve fare il semaforo verde:");
+  while(Serial.available() == 0);
+    if (Serial.available() > 0){
+      inserito = Serial.readString();
+      Serial.println(inserito.toInt());
+      nLampVerde = inserito.toInt();
+      if(nLampVerde > 0){
+        serialeDurataLampVerde();
+      }
+    }
+}
+void serialeDurataLampVerde(){
+  Serial.println("Inserire la durata dei ogni lampeggo del semaforo verde:");
+  while(Serial.available() == 0);
+    if (Serial.available() > 0){
+      inserito = Serial.readString();
+      Serial.println(inserito.toInt());
+      durataLampVerde = inserito.toInt();
+    }
+}
 void Tempo1() {
   digitalWrite(giallo1, LOW);
   digitalWrite(rosso1, HIGH);
@@ -48,7 +89,8 @@ void Tempo1() {
 void Tempo2() {
   digitalWrite(rosso1, HIGH);
   digitalWrite(giallo1, HIGH);
-  digitalWrite(giallo2, HIGH);
+  digitalWrite(giallo2, HIGH);  
+  digitalWrite(verde2, LOW);
 }
 void Tempo3(){
   digitalWrite(rosso1, LOW);
@@ -56,7 +98,6 @@ void Tempo3(){
   digitalWrite(verde1, HIGH);
   digitalWrite(rosso2, HIGH);
   digitalWrite(giallo2, LOW);
-  digitalWrite(verde2, LOW);
 }
 void Tempo4(){  
   digitalWrite(giallo1, HIGH);
@@ -64,20 +105,20 @@ void Tempo4(){
   digitalWrite(giallo2, HIGH);
 }
 void lampVerde1(){
-  for(int i = 0; i < 4; i++){  
+  for(int i = 0; i < nLampVerde; i++){  
     digitalWrite(verde1, LOW);
-    delay(200);
+    delay(durataLampVerde);
     digitalWrite(verde1, HIGH);
-    delay(200);
+    delay(durataLampVerde);
   }
   digitalWrite(verde1, LOW);
 }
 void lampVerde2(){
-  for(int i = 0; i < 4; i++){  
+  for(int i = 0; i < nLampVerde; i++){  
     digitalWrite(verde2, LOW);
-    delay(200);
+    delay(durataLampVerde);
     digitalWrite(verde2, HIGH);
-    delay(200);
+    delay(durataLampVerde);
   }
   digitalWrite(verde1, LOW);
 }
